e99498951c40f90ba8cd737196d34d844ecf86dc
Nguyen Vu <phucnguyenvu0203@gmail.com>
2024-04-22 23:39:19 -0700

    Add level 2 hard mode, did level design + some enemy setup, add hardmode to level select

--- Assets/Asset/Scripts/UI+Menu/LevelSelector.cs
+++ Assets/Asset/Scripts/UI+Menu/LevelSelector.cs
@@ -7,11 +7,18 @@ public class LevelSelector : MonoBehaviour
 {
     public SceneFader Fader;
 
-    public Button[] levelButtons;
+    public GameObject normalMode;
+    public GameObject hardMode;
+    public Button normalButton;
+    public Button hardButton;
 
+    public Button[] levelButtons;
+    public Button[] levelHardButtons;
     void Start()
     {
         int levelReached = PlayerPrefs.GetInt("levelReached", 1);
+
+        normalButton.interactable = false;
 
         for (int i = 0; i < levelButtons.Length; i++)
         {
@@ -20,9 +27,36 @@ public class LevelSelector : MonoBehaviour
                 levelButtons[i].interactable = false;
             }
         }
+
+        for (int i = 0; i < levelHardButtons.Length; i++)
+        {
+            if (i + 1 > levelReached)
+            {
+                levelHardButtons[i].interactable = false;
+            }
+        }
     }
     public void Select(string levelName)
     {
         Fader.FadeTo(levelName);
+    }
+
+    public void LevelToggle()
+    {
+        if (normalMode.activeSelf)
+        {
+            normalMode.SetActive(false);
+            hardMode.SetActive(true);
+            normalButton.interactable = true;
+            hardButton.interactable = false;
+        }
+        
+        else
+        {
+            normalMode.SetActive(true);
+            hardMode.SetActive(false);
+            normalButton.interactable = false;
+            hardButton.interactable = true;
+        }
     }
 }
